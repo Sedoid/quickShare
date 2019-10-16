6183e2e2fef4ca1cdf269d1261f98265dd7965ab
Sedoid <karlsedoide@gmail.com>
2019-10-16 14:47:05 +0100

    fixed some Minor bugs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,58 +7,118 @@ var firebaseConfig = {
     messagingSenderId: "831635016605",
     appId: "1:831635016605:web:04510bad75f4ebc5"
   };
+
+  
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   var firestore = firebase.firestore();
 
   const docRef = firestore.doc("Samples/userInput");
+  const user = '<span style="color:purple; display:block;" >Sedoid</span>';
 
 //console.log(this.Object(docRef));
-
+    const chats = document.querySelector('#chats');
     const header = document.querySelector('h1');
     const userInput = document.querySelector('#userInput');
     const send = document.querySelector('#send');
     const load = document.querySelector('#call');
     const body = document.querySelector('#second');
-          body.style.display = 'none';
+    const clearChat = document.querySelector('#second ul');
 
-    send.addEventListener('click',function(){
-        const textToSave = userInput.value;
-
+ 
+    load.addEventListener('click',function(){
+ 
+        const children = clearChat.childNodes;
+        children.forEach(child =>{
+            clearChat.removeChild(child);
+        })
+         
+    });
+      
 // Sending the data to firebase
+    send.addEventListener('click',function(){
+        const textToSave =user + userInput.value;
+        userInput.value = '';
+
         docRef.set({
-            userInput: textToSave
+            userInput:  textToSave
         }).then(function(event){
             console.log(`Status Saved!`);
         }).catch(function(error){
             console.log(`Got an error ${error}`);
         });
+
    });
-// Loading the data saved on firebase 
-   load.addEventListener('click',function(){
-       console.log('reacting to click event');
-       docRef.get().then(function(doc){
+// Alternative Loading the data saved on firebase 
+//    load.addEventListener('click',function(){
+//         userInput.value = '';
+//         console.log('executing');
+//        docRef.get().then(function(doc){
            
-           if(doc && doc.exists){
-                  
-               const myData = doc.data();
-                 console.log(myData);
-             header.innerHTML = "i've just received: "+ myData.userInput;
-           }
-       }).catch(function(event){
-        console.log('error : '+ event);
-       });
-   });
+//            if(doc && doc.exists){                
+//                const myData = doc.data();           
+           
+//            }
+//        }).catch(function(event){
+//         console.log('error : '+ event);
+//        });
+      
+//    });
+
     
    // enabliing Live loading of data
+   
+   
        const getRealtimeUpdates = function(){
+           let timing = new Date().getTime();
+           console.log("timing is "+timing);
            console.log('it shoud be running now');
            docRef.onSnapshot(function(doc){
                if(doc && doc.exists){
                    const myData = doc.data();
-                   header.innerHTML = "Recieved:"+ myData.userInput;
+// Checking and disposing of scripted messages 
+                   let message = myData.userInput,
+                        tagPosition = message.indexOf('>'),
+                        subMessage = message.substring(0,tagPosition+1);
+                        let newstuff =[...subMessage.trim() ] ;
+                        newstuff[0] = '';
+                        newstuff[newstuff.length-1] = '';
+                       subMessage = newstuff.join('');
+
+                    console.log(subMessage == 'script');
+                   if (subMessage == 'script')
+                   {
+                       let message = myData.userInput;
+                       console.log('garbage collected\n'+ message.substring(0,user.length-1));
+                   }else 
+                  { 
+                      const li = document.createElement('li');
+                         console.log(myData.userInput);
+                        li.innerHTML =  myData.userInput;
+                        textToSave = myData.userInput;
+                        chats.insertBefore(li,chats.childNodes[0]);
+               
+                   }
+                
+                
                }
            });
        }
+// Sending on keyPress 
+       window.onkeydown=function(event){
+           if(event.keyCode==13){
+               send.click();
+               userInput.value = '';
+               console.log('you pressed enter');
+           }
+       };
 
-    getRealtimeUpdates();
+let arr = [];
+
+getRealtimeUpdates();
+   
+function timer(){
+
+}
+
+
